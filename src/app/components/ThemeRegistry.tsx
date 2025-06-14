7d40bfca05f84538b7df2edce371797d070890d4
Mohamed Belal <mohamed.belal@theapsgroup.com>
2025-06-14 08:52:11 +0200

    feat: add mui and theme

---
+++ src/app/components/ThemeRegistry.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+export default function ThemeRegistry({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+}
